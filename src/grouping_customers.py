6d49501abe9767c5ae7fdfc10ba43b58af6dc88a
Ygrick <igor2.004@mail.ru>
2022-10-16 20:50:17 +0300

    Create grouping_customers.py

---
+++ src/grouping_customers.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def groups_customers_1(n_customers, n_first_id=0) -> dict:
+    # инициализация списка пользователей
+    list_customers = list(
+        range(n_first_id, n_customers)
+    )
+    # инициализация словаря для хранения групп
+    dict_customers = dict()
+
+    for item in list_customers:
+        # вычисление суммы цифр
+        str_item = map(int,
+                       str(item)
+                       )
+        count_digit_item = sum(str_item)
+
+        # если группа не создана -> создаём
+        if count_digit_item not in dict_customers.keys():
+            dict_customers[count_digit_item] = list()
+
+        # помещаем пользователя в группу
+        dict_customers[count_digit_item].append(item)
+
+    # сортируем группы по порядку и возвращаем словарь
+    return dict(sorted(dict_customers.items(), key=lambda item: item[1]))
+
+
+if __name__ == "__main__":
+    res = groups_customers_1(int(sys.argv[1]), int(sys.argv[2]))
+    print(res)
+
